a7fd30d1f8a73b1a9cfc8b187dd6ccf135f03eb2
Remus Courtenay <remus.courtenay@betterbusinessbydesign.com>
2023-10-26 15:29:36 +1300

    readded the same old way of doing it with a slightly flashier switch statement

--- CSharp/AccountsReceivable/AccountsReceivable.API/Pages/Invoices.razor.cs
+++ CSharp/AccountsReceivable/AccountsReceivable.API/Pages/Invoices.razor.cs
@@ -1,21 +1,23 @@
 ﻿using AccountsReceivable.BAL.Data;
 using AccountsReceivable.BL.Models.Application;
+using AccountsReceivable.BL.Models.Enum;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace AccountsReceivable.API.Pages;
 
 partial class Invoices
 {
-    private string _searchString = string.Empty;
-    private MudTable<Document> _table = null!;
+    private MudDataGrid<Document> DataGrid;
     private readonly List<BreadcrumbItem> _breadcrumb = new()
     {
         new BreadcrumbItem("Home", ""),
         new BreadcrumbItem("Buyer Created Invoices", null, true)
     };
-
+    
+    private string _searchString = string.Empty;
     private int _totalItems;
 
     [Inject]
@@ -24,7 +26,19 @@ partial class Invoices
     [Inject]
     protected virtual NavigationManager Navigation { get; set; } = default!;
 
-    private async Task<TableData<Document>> ServerReload(TableState state)
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await DataGrid.SetSortAsync(nameof(Document.Status), SortDirection.Ascending, document => document.Status.Name,
+                new NaturalComparer());
+        }
+    }
+    
+
+    private async Task<GridData<Document>> GridServerReload(GridState<Document> state)
     {
         var fullQuery = DbContext.Documents
             .AsNoTracking()
@@ -33,61 +47,62 @@ partial class Invoices
             .Include(document => document.Status)
             .Include(document => document.SpeciesType)
             .Include(document => document.Plant.Meatwork);
-
+        
         var filteredQuery = fullQuery.Where(document =>
             string.IsNullOrWhiteSpace(_searchString) ||
             EF.Functions.Like(document.KillSheet.ToString(), $"%{_searchString}%") ||
             EF.Functions.Like(document.Farm.Name, $"%{_searchString}%") ||
             EF.Functions.Like(document.Plant.Name, $"%{_searchString}%") ||
             EF.Functions.Like(document.Status.Name, $"%{_searchString}%") ||
-            EF.Functions.Like(document.Plant.Meatwork.Name, $"%{_searchString}%") ||
+            //EF.Functions.Like(document.Plant.Meatwork.Name, $"%{_searchString}%") ||
             EF.Functions.Like(
                 document.SpeciesType != null ? document.SpeciesType.DisplayName : "Unknown",
                 $"%{_searchString}%")
         );
 
-        var orderedQuery = state.SortLabel switch
+        var orderedQuery = filteredQuery.OrderBy(document => 0);
+        
+        foreach (var sortDefinition in state.SortDefinitions)
         {
-            "date_field" => filteredQuery.OrderByDirection(state.SortDirection, document => document.DateProcessed),
-            "killsheet_field" => filteredQuery.OrderByDirection(state.SortDirection, document => document.KillSheet),
-            "plant_field" => filteredQuery.OrderByDirection(state.SortDirection,
-                document => document.Plant.Name),
-            "farm_field" => filteredQuery.OrderByDirection(state.SortDirection,
-                document => document.Farm.Name),
-            "species_field" => filteredQuery.OrderByDirection(state.SortDirection,
-                document => document.SpeciesTypeId),
-            "status_field" => filteredQuery.OrderByDirection(state.SortDirection,
-                document => document.StatusId),
-            _ => filteredQuery
-        };
+            orderedQuery = sortDefinition.SortBy switch
+            {
+                "Date" => orderedQuery.ThenBy(document => document.DateProcessed),
+                "KillSheet" => orderedQuery.ThenBy(document => document.KillSheet),
+                "Plant (Works)" => orderedQuery.ThenBy(document => document.Plant.Name),
+                "Farm" => orderedQuery.ThenBy(document => document.Farm.Name),
+                "Species" => orderedQuery.ThenBy(document => document.SpeciesType == null ? string.Empty : document.SpeciesType.DisplayName),
+                "Status" => orderedQuery.ThenBy(document => document.StatusId),
+                _ => throw new NotImplementedException()
+            };
+        }
 
         _totalItems = orderedQuery.Count();
 
-        return new TableData<Document>
+        return new GridData<Document>
         {
             TotalItems = _totalItems,
             Items = await orderedQuery.Skip(state.Page * state.PageSize).Take(state.PageSize).ToArrayAsync()
         };
     }
-
+    
     private void OnSearch(string searchString)
     {
         _searchString = searchString;
-        _table.ReloadServerData();
+        DataGrid.ReloadServerData();
     }
 
-    private void RowClickEvent(TableRowClickEventArgs<Document> clickEvent)
+    private void RowClicked(DataGridRowClickEventArgs<Document> args)
     {
-        Navigation.NavigateTo($"invoices/{clickEvent.Item.Id}");
+        Navigation.NavigateTo($"invoices/{args.Item.Id}");
     }
 
-    private string RowStyleFunc(Document document, int index)
+    private Func<Document, int, string> _rowStyleFunc => (document, i) =>
     {
+        if (document.SpeciesType is null)
+        {
+            return $"background-color: {Colors.DeepOrange.Lighten4}";
+        }
+
         return string.Empty;
-
-        // TODO - Validation
-        return document.SpeciesType == null
-            ? $"background-color: {Colors.DeepOrange.Lighten4};"
-            : string.Empty;
-    }
+    };
 }
